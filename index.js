39c208c0273f5b047743ac53042475737ffcc4c0
KlerKpn <59790939+KlerKpn@users.noreply.github.com>
2020-07-23 06:12:30 +0300

    footer, form

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+const yourUrlGet = '/test.html'
 
-new Vue ({
+const cards = new Vue ({
     el:"#id",
     data:{  
         items: []
     },
     mounted() {
         axios.get("/cards.json").then(response => {
-        this.items = response.data
+        this.items = response.data 
         })
-    }
+    },
+    
+})
+
+$('#load').on('click', function() { 
+    const btn = $(this)
+    const spin = btn.find('div') 
+    $.ajax({
+        url: yourUrlGet,
+        type: "GET",
+        // data: "data",
+        // datatype: "datatype" 
+        beforeSend: function(){
+            btn.attr('disabled', true)
+            spin.css('display','inline-block')
+        },
+        success: function (response) {
+            setTimeout(() => {
+                btn.attr('disabled', false)
+                spin.css('display','none')
+                $('.load-div').before(response)
+            }, 1000)
+        },
+        error: function(){
+            alert('err')
+        }
+    })
 })
